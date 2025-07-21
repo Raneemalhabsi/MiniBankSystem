bf2be34da14a521c98f5b397db378b0886815b1f
Raneem <raneemalhabsi02@gmail.com>
2025-07-21 18:05:21 +0400

    feat: Add deposit and withdraw methods to BankAccount class

--- MiniBankSystem/src/BankAccount.java
+++ MiniBankSystem/src/BankAccount.java
@@ -8,7 +8,30 @@ public class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
-    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+            System.out.println("New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("Withdrawn: " + amount);
+                System.out.println("New balance: " + balance);
+            } else {
+                System.out.println("Insufficient balance.");
+            }
+        } else {
+            System.out.println("Invalid withdraw amount.");
+        }
+    }
+
     public void showAccountInfo() {
         System.out.println("Account Name: " + accountName);
         System.out.println("Account Number: " + accountNumber);
