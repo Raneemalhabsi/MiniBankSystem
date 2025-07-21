2d951cd17d420c1e49c030676c8916a3510ff616
Raneem <raneemalhabsi02@gmail.com>
2025-07-21 17:56:03 +0400

    feat: Add constructor to BankAccount class

--- MiniBankSystem/src/BankAccount.java
+++ MiniBankSystem/src/BankAccount.java
@@ -3,6 +3,12 @@ public class BankAccount {
     String accountNumber;
     double balance;
 
+    public BankAccount(String accountName, String accountNumber, double balance) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    
     public void showAccountInfo() {
         System.out.println("Account Name: " + accountName);
         System.out.println("Account Number: " + accountNumber);
